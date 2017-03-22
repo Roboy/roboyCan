a8fcc7b443044270d80b677a6a12062495ae7982
Raphael Dürscheid <rd@generalinterfaces.com>
2017-03-22 10:28:46 +0100

    Add check for double motor entries in yaml

--- include/roboy_can/motorsParser.hpp
+++ include/roboy_can/motorsParser.hpp
@@ -8,21 +8,29 @@
 #include <numeric>
 
 struct CanIdNetworktuple {
-  unsigned int id;
+  unsigned int canId;
   std::string network;
 };
 using motorNames = std::map<std::string, CanIdNetworktuple>;
 using motorNamesVariant =
-    variant<empty<motorNames>, motorNames, missing<motorNames>>;
+    variant<empty<motorNames>, motorNames, missing<motorNames>,
+            duplicate<CanIdNetworktuple, std::string>>;
 
 inline auto growMotorNames(motorNames previous,
                            YAML::const_iterator::value_type subnet)
     -> motorNamesVariant {
-  std::string networkName = subnet.first.as<std::string>();
+  std::set<unsigned int> unique;
 
   for (auto &mname : subnet.second) {
-    previous[mname.second["id"].as<std::string>()] = {
-        mname.first.as<unsigned int>(), networkName};
+    CanIdNetworktuple tuple = {mname.first.as<unsigned int>(),
+                               subnet.first.as<std::string>()};
+
+    if (unique.emplace(tuple.canId).second == false) {
+      std::stringstream temp;
+      temp << tuple.network << ": " << tuple.canId;
+      return duplicate<CanIdNetworktuple, std::string>{temp.str()};
+    }
+    previous[mname.second["id"].as<std::string>()] = tuple;
   }
   return {previous};
 };
@@ -45,7 +53,9 @@ template <> struct convert<motorNamesVariant> {
               [&subnet](motorNames previous) -> motorNamesVariant {
                 return growMotorNames(previous, subnet);
               },
-              passAlong<missing<motorNames>, motorNamesVariant>{});
+              passAlong<missing<motorNames>, motorNamesVariant>{},
+              passAlong<duplicate<CanIdNetworktuple, std::string>,
+                        motorNamesVariant>{});
         });
     return true;
   };
--- test/ParserMotorsTest.cpp
+++ test/ParserMotorsTest.cpp
@@ -7,7 +7,10 @@ TEST(roboy_yaml, parseMotorNames) {
   mnv.match(
       [](empty<motorNames>) -> void { FAIL() << "empty<motorNames>"; },
       [](motorNames mn) -> void { EXPECT_EQ(mn.size(), 11); },
-      [](missing<motorNames>) -> void { FAIL() << "missing<motorNames>"; });
+      [](missing<motorNames>) -> void { FAIL() << "missing<motorNames>"; },
+      [](duplicate<CanIdNetworktuple, std::string>) -> void {
+        FAIL() << "duplicate<CanIdNetworktuple, std::string>";
+      });
 }
 
 TEST(roboy_yaml, MotorNamesValue) {
@@ -16,12 +19,27 @@ TEST(roboy_yaml, MotorNamesValue) {
   mnv.match(
       [](empty<motorNames>) -> void { FAIL() << "empty<motorNames>"; },
       [](motorNames mn) -> void {
-        EXPECT_EQ(mn.at("BriskBugs").id, 1);
+        EXPECT_EQ(mn.at("BriskBugs").canId, 1);
         EXPECT_EQ(mn.at("BriskBugs").network, std::string("Left"));
-        EXPECT_EQ(mn.at("ChosenPlants").id, 34);
+        EXPECT_EQ(mn.at("ChosenPlants").canId, 34);
         EXPECT_EQ(mn.at("ChosenPlants").network, std::string("Left"));
-        EXPECT_EQ(mn.at("GoodLemurs").id, 41);
+        EXPECT_EQ(mn.at("GoodLemurs").canId, 41);
         EXPECT_EQ(mn.at("GoodLemurs").network, std::string("Right"));
       },
-      [](missing<motorNames>) -> void { FAIL() << "missing<motorNames>"; });
+      [](missing<motorNames>) -> void { FAIL() << "missing<motorNames>"; },
+      [](duplicate<CanIdNetworktuple, std::string>) -> void {
+        FAIL() << "duplicate<CanIdNetworktuple, std::string>";
+      });
+}
+
+TEST(roboy_yaml, DuplicateMotorName) {
+  auto const node = YAML::LoadFile("roboy_duplicate_motor.yaml");
+  motorNamesVariant mnv = node["Maxon"].as<motorNamesVariant>();
+  mnv.match(
+      [](empty<motorNames>) -> void { FAIL() << "empty<motorNames>"; },
+      [](motorNames mn) -> void { FAIL() << "motorNames mn"; },
+      [](missing<motorNames>) -> void { FAIL() << "missing<motorNames>"; },
+      [](duplicate<CanIdNetworktuple, std::string> dd) -> void {
+        EXPECT_EQ(dd.key, std::string("Left: 34"));
+      });
 }
